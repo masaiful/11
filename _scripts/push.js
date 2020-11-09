386587e5664f9cfc837b13d65ee812d72acdb04d
Nikhil Anand <mail@nikhil.io>
2020-11-08 19:16:31 -0600

    2020-11-08T19.16.24

--- _scripts/push.js
+++ _scripts/push.js
@@ -3,14 +3,35 @@ const { exec } = require("child_process");
 
 const config = require("./eleventy/config");
 
-const command = `
+/* --- */
+
+const compressFlag = "--content-encoding gzip";
+const compressCommand = `
+    find ${config.SITE.dir.output}/ \
+        -type f \
+        -not -iname "search-data.json" \
+        -exec gzip -9 \'{}\' \\; -exec mv \'{}.gz\' \'{}\' \\;
+`;
+
+if (process.env.CI) {
+  console.log(chalk.yellow("Compressing output"));
+
+  const compress = exec(compressCommand);
+  compress.stdout.on("data", (data) => console.log(chalk.green(data)));
+  compress.stderr.on("data", (data) => console.log(chalk.red(data)));
+}
+
+/* --- */
+
+const syncCommand = `
 aws s3 sync ${config.SITE.dir.output}/ "s3://${config.BUCKET_NAME}/" \
     --exclude "${config.MISC_PATH}/*" \
-    --delete
+    --delete \
+    ${process.env.CI ? compressFlag : ""}
 `;
 
 console.log(chalk.yellow(`Publishing site to s3://${config.BUCKET_NAME}`));
 
-const push = exec(command);
-push.stdout.on("data", (data) => console.log(chalk.green(data)));
-push.stderr.on("data", (data) => console.log(chalk.red(data)));
+const sync = exec(syncCommand);
+sync.stdout.on("data", (data) => console.log(chalk.green(data)));
+sync.stderr.on("data", (data) => console.log(chalk.red(data)));
