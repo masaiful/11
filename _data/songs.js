e88503887370ae8f975a54513df654f8a18e4604
Nikhil Anand <nikhilanand@granular.ag>
2020-12-29 15:40:27 -0600

    2020-12-29T15.40.23

--- _data/songs.js
+++ _data/songs.js
@@ -1,6 +1,3 @@
-/**
- * Make things easy to edit on my phone.
- */
 const SONGS = `
 # Track
 # Artist
@@ -26,10 +23,941 @@ Last Night The Moon Came
 John Hassell
 https://www.youtube.com/watch?v=-7IRnin9gF0
 Via Catherine <3
+---
+Spiegel im Spiegel
+Arvo Part
+https://www.youtube.com/watch?v=QtFPdBUl7XQ
+ethereal, classical
+---
+I'm Shakin'
+Little Willie John
+https://www.youtube.com/watch?v=qWRjus3end4
+oldie
+---
+Self Esteem Fund
+Portal
+https://www.youtube.com/watch?v=IyPb-8OjWVk
+soundtrack, games
+---
+Cigarettes & Chocolate Milk
+Rufus Wainwright
+https://www.youtube.com/watch?v=d5CLmflrwIA
+memories, iowa city
+---
+Evolution - Walking On Fire
+John Digweed
+https://www.youtube.com/watch?v=_byizl4VxPI
+electronic, moody
+---
+Man Of Constant Sorrow
+Kraak & Smaak
+https://www.youtube.com/watch?v=Pkzs-tjc5EY
+
+---
+Gun
+Emiliana Torrini
+https://www.youtube.com/watch?v=j29Xwh9_RXE
+
+---
+1hr Alpha Binaural Beat Session (12hz)
+Pure
+https://www.youtube.com/watch?v=7cHjaJRl1Qo
+
+---
+Eyeball Kid
+Tom Waits
+https://www.youtube.com/watch?v=XtXXmn0NS40
+
+---
+Requiem in C minor
+Cherubini
+https://www.youtube.com/watch?v=laO7Zi6Zsm8
+
+---
+Vellipothe Ela
+
+https://www.youtube.com/watch?v=q8iUbrQRF-Y
+telugu, soundtrack
+---
+Moula Saeen
+
+https://www.youtube.com/watch?v=jrmFgKc9-Tg
+
+---
+Na Jaane Kyon
+Strings
+https://www.youtube.com/watch?v=2M2FHg9-V2U
+pakistan
+---
+It Overtakes Me
+The Flaming Lips
+https://www.youtube.com/watch?v=B4O7aI87E3g
+
+---
+Washed Out
+Eyes Be Closed
+https://www.youtube.com/watch?v=Cj2HcdiOmt8
+favorite
+---
+Potter's Village
+A R Rahman
+https://www.youtube.com/watch?v=9n8wuLepiFQ
+favorite
+---
+Running To The Sea feat. Susanne Sundfør (Live on Lydverket)
+Röyksopp
+https://www.youtube.com/watch?v=AMlJ78-7rZ4
+
+---
+Oruvaan Irruvaan
+A R Rahman
+https://www.youtube.com/watch?v=JGNYXqjqyg0
+
+---
+Naina Thag Lenge
+Vishal Bhardwaj
+https://www.youtube.com/watch?v=dGwNTPTmhoQ
+
+---
+Guncha Koi
+Mohit Chauhan
+https://www.youtube.com/watch?v=fC52GtvhRj4
+hindi, favorite
+---
+Kula Shaker
+Timeworm
+https://www.youtube.com/watch?v=c34XLYsr2EQ
+favorite, trippy
+---
+O Fortuna (Carmina Burana)
+Orff
+https://www.youtube.com/watch?v=BNWpZ-Y_KvU
+classical
+---
+In the Hall of the Mountain King - The Social Network
+Trent Reznor
+https://www.youtube.com/watch?v=cD8EPdn5Ctg
+soundtrack, favorite
+---
+The Brandt Brauer Frick Ensemble
+The Brandt Brauer Frick Ensemble
+https://www.youtube.com/watch?v=lrOVaqdYxuM
+favorite
+---
+Dissolved Girl
+Massive Attack
+https://www.youtube.com/watch?v=OT8QT4BEOTo
+trip hop, favorite
+---
+Bloom (The King of Limbs)
+Radiohead
+https://www.youtube.com/watch?v=8A9bMTh9rdQ
+favorite, moody
+---
+The Sunshine Underground
+The Chemical Brothers
+https://www.youtube.com/watch?v=IykWbUVDWL8
+electronic
+---
+Tomorrow Never Knows
+The Beatles
+https://www.youtube.com/watch?v=tisjsgsgtZU
+
+---
+School of Braja - Braj Mandala
+Kula Shaker
+https://www.youtube.com/watch?v=BpjWvpTYfSw
+
+---
+Home
+LCD Soundsystem
+https://www.youtube.com/watch?v=8Sokz0oy7wo
+
+---
+I Can Change
+LCD Soundsystem
+https://www.youtube.com/watch?v=tW8FKkVnqng
+favorite
+---
+Because We Can
+Fatboy Slim
+https://www.youtube.com/watch?v=rjIl_xFUruM
+electronic, sountrack
+---
+Herran
+Chemical Brothers & Moby vs. Fatboy Slim & Prodigy
+https://www.youtube.com/watch?v=Ir4n0Re8scg
+electronic
+---
+Hang Me Up To Dry
+Cold War Kids
+https://www.youtube.com/watch?v=LrrGKR8Xii4
+
+---
+Escape 700
+The Chemical Brothers
+https://www.youtube.com/watch?v=qqKnoXXL-yI
+
+---
+Black Sands
+Bonobo
+https://www.youtube.com/watch?v=Ua2loiGHZ38
+favorite
+---
+Full Moon
+The Black Ghosts
+https://www.youtube.com/watch?v=4A1wkErAilU
+favorite
+---
+Beedi
+Omkara
+https://www.youtube.com/watch?v=FDHke6gJWEg
+hindi, indian
+---
+Habibi (Club Mix)
+Amr Diab
+https://www.youtube.com/watch?v=Hc3Jxx6dbJo
+
+---
+Mera Yaar Mila De
+A R Rahman
+https://www.youtube.com/watch?v=toHbL52fDGw
+favorite
+---
+Kuchi Kuchi Rakkamma
+A R Rahman
+https://www.youtube.com/watch?v=1L0Q7JOF_bA
+
+---
+How It Ends
+DeVotchKa
+https://www.youtube.com/watch?v=Pfi1UQ_PKQI
+favorite
+---
+Nyah and Ethan
+Hans Zimmer
+https://www.youtube.com/watch?v=H4zo7-XC3IY
+soundtrack, favorite
+---
+Block Rockin Beats (The Micronauts remix)
+The Chemical Brothers
+https://www.youtube.com/watch?v=RTyqEzWUavU
+favorite
+---
+AirTap
+Erik Mongrain
+https://www.youtube.com/watch?v=AbndgwfG22k
+guitar, amazing
+---
+Lotus Flower
+Radiohead
+https://www.youtube.com/watch?v=cfOa1a8hYP8
+
+---
+Baby Did a Bad Bad Thing
+Chris Isaak
+https://www.youtube.com/watch?v=dPZQ7ZLy3MI
+
+---
+Tick Of The Clock
+Chromatics
+https://www.youtube.com/watch?v=vWD7k6TrJ-g
+
+---
+The Girl from Ipanema
+Astrud Gilberto, João Gilberto and Stan Getz
+https://www.youtube.com/watch?v=c5QfXjsoNe4
+
+---
+Muchas Gracias
+Medeski, Martin & Wood
+https://www.youtube.com/watch?v=jWewv3bsFdI
+
+---
+Ode To The Big Sea (Four Tet Remix)
+The Cinematic Orchestra
+https://www.youtube.com/watch?v=RakC7ToUm08
+
+---
+Your Woman
+White Town
+https://www.youtube.com/watch?v=HIX2RAHPTsI
+
+---
+My Comfort Remains
+Nusrat Fateh Ali Khan
+https://www.youtube.com/watch?v=ogz-sygS2Fs
+
+---
+Pompeii Am Götterdämmerung
+The Flaming Lips
+https://www.youtube.com/watch?v=wKbJpngxzc0
+
+---
+Block Rockin Beats
+The Chemical Brothers
+https://www.youtube.com/watch?v=MaU5qkQJ1e4
+
+---
+Hairy Candy
+Tobacco
+https://www.youtube.com/watch?v=ygHbrjLSaZw
+favorite
+---
+Surrender
+The Chemical Brothers
+https://www.youtube.com/watch?v=FlrzmuNqzUU
+
+---
+Too Many Kids Finding Rain In The Dust (Matt Gill Edit)
+Nicolas Jaar
+https://www.youtube.com/watch?v=brI-SdN_XUs
+
+---
+Surrender
+Petalpusher
+https://www.youtube.com/watch?v=xnKWAWh3tHI
+
+---
+Stay Don't Go
+Spoon
+https://www.youtube.com/watch?v=K7SENr7EGDE
+
+---
+Container Park (Hanna)
+The Chemical Brothers
+https://www.youtube.com/watch?v=HBmwcuLa1as
+soundtrack
+---
+Diablo Rojo - Live
+Rodrigo y Gabriela
+https://www.youtube.com/watch?v=PT9hvyDvKHA
+
+---
+Hora Zero
+Rodrigo y Gabriela
+https://www.youtube.com/watch?v=LRSOfQRHu7o
+
+---
+La Femme D'Argent
+Air
+https://www.youtube.com/watch?v=VH5bL_XbO64
+
+---
+Return of Shankara
+Krome Assassins
+https://www.youtube.com/watch?v=MbGbH5fmnQM
+favorite
+---
+Dil Se Re
+A R Rahman
+https://www.youtube.com/watch?v=YwfCMvo19s8
+
+---
+Om Keshavaya Namah
+Kula Shaker
+https://www.youtube.com/watch?v=Jj_FBOx6eHw
+
+---
+Radhe Krishna
+State of Bengal and Paban Das Baul
+https://www.youtube.com/watch?v=8ih3_n_0I8U
+favorite, beautiful
+---
+Radhe Radhe
+Kula Shaker
+https://www.youtube.com/watch?v=RDM3CSdK4vk
+
+---
+Govinda
+Kula Shaker
+https://www.youtube.com/watch?v=2SyVaLpa_vc
+
+---
+Kailash Kher
+Allah ke Bande
+https://www.youtube.com/watch?v=9Ered22Xy4E
+
+---
+Saturday Come Slow
+Massive Attack
+https://www.youtube.com/watch?v=JnE8dxbonDQ
+
+---
+Samba Do Gringo Paulista
+Suba
+https://www.youtube.com/watch?v=eGZfd5lRrBw
+
+---
+Imidiwan Ma Tenam on the Colbert Report
+Tinariwen
+https://www.youtube.com/watch?v=vtNm1uKvELE
+
+---
+Caje Sukarije
+Goran Bregovic
+https://www.youtube.com/watch?v=xahHvuiXows
+
+---
+Underground Cocek
+Goran Bregovic
+https://www.youtube.com/watch?v=HurHtBAydHQ
+
+---
+Ringe Ringe Raja
+Goran Bregovic
+https://www.youtube.com/watch?v=VaHFTLxenog
+
+---
+Tomorrow Never Knows
+The Beatles
+https://www.youtube.com/watch?v=6a3NcwfOBzQ
+
+---
+Pyramid Song - Der Himmel uber Berlin
+Radiohead
+https://www.youtube.com/watch?v=ee05p1I464s
+
+---
+Peregrino
+Zuco 103
+https://www.youtube.com/watch?v=Kurcj_32wSQ
+
+---
+Hey Boy Hey Girl
+The Chemical Brothers
+https://www.youtube.com/watch?v=5wlz9JD-WtI
+
+---
+Breathe
+The Prodigy
+https://www.youtube.com/watch?v=6_PAHbqq-o4
+
+---
+Firestarter
+The Prodigy
+https://www.youtube.com/watch?v=zTYRGi-Q7U4
+
+---
+Hand Covers Bruise (The Social Network)
+Trent Reznor
+https://www.youtube.com/watch?v=1Ua2gabdJoc
+
+---
+In Motion (The Social Network)
+Trent Reznor and Atticus Ross
+https://www.youtube.com/watch?v=Yczul_609Gg
+
+---
+Paradise Circus
+Massive Attack
+https://www.youtube.com/watch?v=jEgX64n3T7g
+
+---
+Nanati Bratuku Natakamu
+Annamacharya
+https://www.youtube.com/watch?v=k3thpH48yAU
+
+---
+Beedi Jalaile
+Omkara
+https://www.youtube.com/watch?v=p8mab5DJU5Y
+bollywood
+---
+Superpredators
+Massive Attack
+https://www.youtube.com/watch?v=tzSiL5MsPAQ
+
+---
+Tera Chehra Sad
+Adnan Sami
+https://www.youtube.com/watch?v=DL_kQJQBKmQ
+
+---
+The Time We Lost Our Way
+Thievery Corporation
+https://www.youtube.com/watch?v=KTHFXeKLv6w
+
+---
+Personal Jesus (Alex Metric Remix 2011)
+Depeche Mode
+https://www.youtube.com/watch?v=5pmk7HtN80k
+
+---
+Jolene
+The White Stripes
+https://www.youtube.com/watch?v=H4rYaLBUpLA
+
+---
+Will Do
+TV On The Radio
+https://www.youtube.com/watch?v=dXLpXu9T7j0
+
+---
+Nuba 1
+A. Cyrille & J. Lee & J. Lyons
+https://www.youtube.com/watch?v=eUF2vf3Wt8A
+jazz, crazy, eccentric, beautiful
+---
+Nostalgia
+Emily Barker & The Red Clay Halo
+https://www.youtube.com/watch?v=bNtZR8gR9aQ
+
+---
+Carnival Of Animals - The Swan
+Camille Saint-Saëns
+https://www.youtube.com/watch?v=NmMAlLfwirY
+classical
+---
+Human Cannonball
+Butthole Surfers
+https://www.youtube.com/watch?v=TK8CNSciKp4
+
+---
+Quiet City
+Pan American
+https://www.youtube.com/watch?v=O1JhZN9QGoY
+ambient, 2004
+---
+Het Volk
+Pan American
+https://www.youtube.com/watch?v=RsnNVr5n9yk
+the saddest song in the world
+---
+Fools Gold
+Stone Roses
+https://www.youtube.com/watch?v=BBsazIACpYM
+
+---
+Thakita Tadhimi
+Ilayaraja
+https://www.youtube.com/watch?v=M3Mb9yOaaT0
+telugu, beautiful
+---
+In the Hall of the Mountain King (The Social Network)
+Trent Reznor and Atticus Ross
+https://www.youtube.com/watch?v=cD8EPdn5Ctg
+
+---
+Because We Can
+Fatboy Slim
+https://www.youtube.com/watch?v=rjIl_xFUruM
+
+---
+99 Problems
+Jay-Z
+https://www.youtube.com/watch?v=WwoM5fLITfk
+
+---
+Insomnia (Lulu Rouge bootleg)
+Faithless
+https://www.youtube.com/watch?v=m9n2g3wm0mo
+
+---
+M83 Vs. Daftpunk Vs. M.In (Lulu Rouge Bootleg)
+
+https://www.youtube.com/watch?v=2CahAlCcBTM
+
+---
+I Heard It Through The Grapevine
+Gladys Knight and the Pips
+https://www.youtube.com/watch?v=WWvwP72FuVg
+
+---
+Romano Song (feat. Annisette Koppel)
+Lulu Rouge
+https://www.youtube.com/watch?v=109m0zeAb7o
+
+---
+Feeling Good live Radio 3
+Muse
+https://www.youtube.com/watch?v=5fIFnFUyMnI
+
+---
+Ghosts I - 2
+Nine Inch Nails
+https://www.youtube.com/watch?v=T6R8GCX_wSs
+
+---
+Grey Heron Man
+Lulu Rouge
+https://www.youtube.com/watch?v=miCf9R4LiWQ
+
+---
+Man O To (original Mix)
+Nu
+https://www.youtube.com/watch?v=2M1xKXd4GVI
+
+---
+Saeen
+Usman Riaz
+https://www.youtube.com/watch?v=-s24jnx-xbM
+pakistan, beautiful, sufi
+---
+Mahatma
+Dan Holland
+https://www.youtube.com/watch?v=7-mLIj5IrEQ
+
+---
+Chicken in a Box
+Mr. Scruff
+https://www.youtube.com/watch?v=M96t_SMIoPQ
+
+---
+Subterraneans (Part 1)
+Philip Glass
+https://www.youtube.com/watch?v=oxhIkc5gthI
+
+---
+La Fugitive
+Williams Traffic
+https://www.youtube.com/watch?v=l_e0Xx5vWZE
+
+---
+Daggasse
+The Gaslamp Killer
+https://www.youtube.com/watch?v=ue7jyNgoDiA
+
+---
+Regenmacher / Audio (1978)
+Roedelius
+https://www.youtube.com/watch?v=BVI327Y3Xko
+weird, awesome, vintage
+---
+Noir & Haze - Around [Solomun Vox Mix] - NMB037
+
+https://www.youtube.com/watch?v=7bMYhJ_UqnE
+
+---
+Thaniye Thananthaniye
+A R Rahman
+https://www.youtube.com/watch?v=4v4gJ_Z8SDo
+
+---
+Homeostasis
+Nostalghia
+https://www.youtube.com/watch?v=VCMWXPCbc2E
+Heard this in "How to get away with Murder"
+---
+Chok There
+Apache Indian
+https://www.youtube.com/watch?v=iUK7MeLIcXA
+
+---
+Hung Up
+Madonna
+https://www.youtube.com/watch?v=EDwb9jOVRtU
+
+---
+No Sugar In My Coffee
+Caught A Ghost
+https://www.youtube.com/watch?v=NnEnHYL1whA
+
+---
+Tide
+Junip
+https://www.youtube.com/watch?v=GLDtFaU3KUY
+
+---
+Shivam (Baahubali 2)
+M M Keeravani
+https://www.youtube.com/watch?v=MxXpPqRG2ZU
+
+---
+Weightless
+Marconi Union
+https://www.youtube.com/watch?v=UfcAVejslrU
+favorite
+---
+Oka Praanam (Baahubali)
+M M Keeravani
+https://www.youtube.com/watch?v=cCQ-NBwzAFw
+
+---
+"A Westerner beats the Indian Drums"
+Unknown
+https://www.youtube.com/watch?v=bnVqv9-qg6o
+"This is NOT Mridangam. This is Tavil."
+---
+Punjabi Girl with Drum
+Unknown
+https://www.youtube.com/watch?v=yco00wAXCuc
+punjabi
+---
+Shape of You (Bhangra Remix)
+Ed Sheeran, Manraj Khaira
+https://www.youtube.com/watch?v=CIU4gHLefxo
+punjabi
+---
+In The Shadow Of Life (Jerf Remix)
+Niyaz
+https://www.youtube.com/watch?v=jPNP1tkgI_k
+
+---
+Abglanz
+Pantha Du Prince
+https://www.youtube.com/watch?v=Vx6CN-gCS6s
+
+---
+Wade In The Water
+Ella Jenkins
+https://www.youtube.com/watch?v=vg_8L96E3eU
+
+---
+Sawaar Loon (Lootera)
+Amit Trivedi
+https://www.youtube.com/watch?v=6k8Aja80GQM
+
+---
+Aik Alif
+Noori & Saeen Zahoor
+https://www.youtube.com/watch?v=Ra5nTlty6CM
+Coke Studio
+---
+O Majhi Re
+Shaan & Saurav Moni
+https://www.youtube.com/watch?v=X-CKZCqH6IQ
+Coke Studio S01E01
+---
+I Alla Al'Hbab
+DJ Cheb I Sabbah
+https://www.youtube.com/watch?v=XFLudFZ4bWA
+
+---
+Death Letter Blues
+Son House
+https://www.youtube.com/watch?v=NdgrQoZHnNY
+
+---
+Ain't No Sunshine
+Bill Withers
+https://www.youtube.com/watch?v=HBKcAc8VpIw
+
+---
+Teen Mar
+Unknown
+https://www.youtube.com/watch?v=P30tlILnKQA
+instrumental
+---
+Nadhaswaram - Classical Instrumental - Abheri
+Jayashankar & Valayapatti Subramaniam
+https://www.youtube.com/watch?v=J5dwWRFd8f4
+instrumental, indian classical
+---
+Orchestrated Incident
+Gramatik
+https://www.youtube.com/watch?v=KK2zeLbKlnc
+Street Bangerz Vol. 2
+---
+Marhaba (Janasheen)
+Sukhwinder Singh
+https://www.youtube.com/watch?v=X4dEtGKQc5k
+
+---
+Hey Boy Hey Girl
+The Chemical Brothers
+https://www.youtube.com/watch?v=Bp0rQIHWFWI
+
+---
+Breathe
+Télépopmusik
+https://www.youtube.com/watch?v=vyut3GyQtn0
+
+---
+Qawwali Aaj Rang Hain
+Bajrangi Bhaijaan
+https://www.youtube.com/watch?v=yEQuTfNVmG0
+
+---
+Dheem Ta Dare
+A R Rahman
+https://www.youtube.com/watch?v=nCFpPgF1xKQ
+
+---
+Indian Drum Vs African Drum
+Unknown
+https://www.youtube.com/watch?v=PS-Y8jCiKSk
+
+---
+Daf Solo
+Hussein Zahawy
+https://www.youtube.com/watch?v=KboogWwmzrc
+
+---
+MERU Concert
+Vikku Vinayakram
+https://www.youtube.com/watch?v=aTLIkF4yxBc
+
+---
+Beautiful Gypsy Violin
+Unknown
+https://www.youtube.com/watch?v=sHPBCLnLAWY
+
+---
+Ghul Raha Hai Sara Manzar
+Shankar Mahadevan
+https://www.youtube.com/watch?v=7kBcU1F8y78
+
+---
+Arrival of The Birds & Transformation
+The Cinematic Orchestra
+https://www.youtube.com/watch?v=n88MReEC27k
+
+---
+November
+Max Richter
+https://www.youtube.com/watch?v=2Bb0k9HgQxc
+
+---
+On the Nature of Daylight
+Max Richter
+https://www.youtube.com/watch?v=rVN1B-tUpgs
+
+---
+Personal Jesus
+Marilyn Manson
+https://www.youtube.com/watch?v=Rl6fyhZ0G5E
+
+---
+Bombay Theme (with Berklee Ensemble)
+A R Rahman
+https://www.youtube.com/watch?v=fgjjJOZkXbQ
+
+---
+Le Petit Prince
+Sahalé
+https://www.youtube.com/watch?v=OZbagrROhn0
+
+---
+Let's Live For Today
+Nicolas Jaar
+https://www.youtube.com/watch?v=grrrBeY31wk
+
+---
+Dil Se Re Cover
+Dhrubajyoti Dhruv
+https://www.youtube.com/watch?v=PlRFg4Mo3Bw
+
+---
+Aadat Unplugged
+Atif Aslam
+https://www.youtube.com/watch?v=Q36nyOph95M
+
+---
+Qalanderi
+DJ Cheb I Sabbah
+https://www.youtube.com/watch?v=UXgKcBsQTL8
+
+---
+The Struggle
+The Dead Texan
+https://www.youtube.com/watch?v=oAIg9pY4FDA
+moody
+---
+Roja - Kyiv Classic Orchestra
+A R Rahman
+https://www.youtube.com/watch?v=6gJfYdPJZRM
+
+---
+A Dead Mouse
+MF Doom
+https://www.youtube.com/watch?v=leVETb-XagQ
+hiphop, favorite
+---
+Open Mic Nite, Pt. 1
+MF Doom
+https://www.youtube.com/watch?v=2OxMR-ccZyo
+hiphop
+---
+Illegal Weapon
+Garry Sandhu and Jasmine Sandlas
+https://www.youtube.com/watch?v=H7_yY3yr-jE
+punjabi
+---
+Padara Padara
+Shankar Mahadevan
+https://www.youtube.com/watch?v=di7xdyhcboc
+telugu
+---
+Undiporaadhey
+Sid Sriram
+https://www.youtube.com/watch?v=lewVFlngGCk
+telugu
+---
+Ocean Bloom
+Hans Zimmer & Radiohead
+https://www.youtube.com/watch?v=Hv8kSWyIKsM
+
+---
+Closer
+Kerala Dust
+https://www.youtube.com/watch?v=45WdCpZ0Szo
+favorite
+---
+Lover, You Should've Come Over
+Jeff Buckley
+https://www.youtube.com/watch?v=hXe1jpHPnUs
+
+---
+Makeba
+Jain
+https://www.youtube.com/watch?v=ryG4CSQ_aJE
+
+---
+Monkey Drums (Copenhagen Jazz Festival Bootleg)
+Lulu Rouge
+https://www.youtube.com/watch?v=B22l4dGjgME
+
+---
+Nobody Listens to Techno (Unreleased Deep Dish Mix)
+Eminem
+https://www.youtube.com/watch?v=HXCYMfdjNTY
+
+---
+Pyaar Karke Pachtaya
+Pyaar Ke Side Effects
+https://www.youtube.com/watch?v=QBvaNjkq_28
+funny, favorite, punjabi
+---
+Makeba (Extended Mix)
+Sllash & Doppe
+https://www.youtube.com/watch?v=qelfzbA7x8w
+dance
+---
+Tribute to Keith Flint
+The Prodigy Orchestra Medley
+https://www.youtube.com/watch?v=h7Xz1J5tmGs
+classical
+---
+Daryaa Unplugged
+Deveshi Sahgal
+https://www.youtube.com/watch?v=1ict9pqMEhk
+favorite, cover
+---
+Music
+Erick Sermon feat. Marvin Gaye
+https://www.youtube.com/watch?v=fle-zebSXNc
+
+---
+Maya Machindra
+A R Rahman
+https://www.youtube.com/watch?v=wtT-z1Dqb-A
+
+---
+AKHNATEN
+Philip Glass
+https://www.youtube.com/watch?v=jSAOrULT-F4
+classical
+---
+The Night Josh Tillman Came To Our Apartment
+Father John Misty
+https://www.youtube.com/watch?v=iY37fixfN_A
+funny, via Catherine <3
 `;
 
 /**
- * Parse the above. It's a parser, OK? A shitty one but one nonetheless <3
+ * Parse the above. It's a parser, OK?
+ * A shitty one but one nonetheless <3
  */
 const generate = (content) => {
   const ret = content
