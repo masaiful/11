f2d66a574c9b9b6fd835820d14fb228867417f67
masaiful <43263643+masaiful@users.noreply.github.com>
2021-02-13 13:27:12 +0100

    Update site.js

--- _data/site.js
+++ _data/site.js
@@ -1,9 +1,8 @@
 module.exports = {
-  name: "log.nikhil.io",
-  description: "Nikhil's Log",
+  name: "Saiful Muhajir",
+  description: "just another viewpoint",
   locale: "en-US",
-  url: "https://log.nikhil.io",
-  googleAnalyticsId: "UA-88882746-3",
-  googleSearchConsoleId: "zZ8S_r0B6ZCxgP_yPsKkCxHhirltYL_hS-liqhjjlg8",
+  url: "https://saifulmuhajir.web.id",
+  googleSearchConsoleId: "",
   environment: process.env.ELEVENTY_ENV,
 };
